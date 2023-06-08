e8cb533d9bfe1f998eea0985a6e5c4cc53e394ae
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-08 12:09:21 +0900

    Feat : 머신러닝과빅데이터분석(2주차)_R프로그래밍 #2 - iris, apply, lapply, sapply

--- R_MachineLearning&BigData_2.R
+++ R_MachineLearning&BigData_2.R
@@ -79,3 +79,49 @@ for (i in 2 : 9) {
         print(i * j)
     }
 }
+
+# 6. apply함수 : 벡터, 행렬, 리스트에서 행/열 단위연산
+# apply(행렬, margin, 적용함수)
+# margin 1 : 행연산 margin 2 : 열연산
+
+# matrix : 2차원데이터
+# matrix(1 : 20, 행수, 열수)
+apply(matrix(1 : 6, 2, 3), 1, sum)
+apply(matrix(1 : 6, 2, 3), 2, sum)
+apply(matrix(1 : 6, 2, 3), 2, min)
+
+matrix(1 : 6, 2, 3) # 행이 2개 열이 3개
+
+# 7. iris ; 내장함수_꽃데이터(꽃받침, 꽃잎길이, 너비, 종)
+# iris데이터 -> 알고리즘적용 -> 분류종모델
+# 새로운 iris 데이터 입력 -> 모델이 분류해서 결과도출
+iris
+
+# 7_1. str()함수 : 데이터 구조 알려줌
+str(iris)
+
+# data.frame = 엑셀시트
+# 옵저베이션 obs : 관측값 (데이터하나하나)
+
+# data.frame vs matrix
+# - 공통점 : 2차원
+# - 차이점 : matrix -> 데이터타입동일, data.frame -> 데이터타입제한없음
+
+# 7_2. (데이터프레임명)$(변수명)
+iris$Sepal.Length
+
+# 7_3. View(데이터프레임명) : 표형태로 데이터값 보여줌
+View(iris)
+
+# 7_4. iris 데이터프레임 가공하기
+apply(iris[,1 : 4], 1, sum) # iris[추출할 행, 열]
+apply(iris[,1 : 4], 2, sum)
+apply(iris[,1 : 4], 2, median) # 중앙값
+apply(iris[,1 : 4], 2, max)
+
+# 7_5. lapply : 결과를 List로 리턴
+# lapply(데이터, 함수)
+lapply(iris[,1 : 4], sum)
+
+# 7_5. sapply : 결과를 벡터로 리턴
+sapply(iris[,1 : 4], sum)
