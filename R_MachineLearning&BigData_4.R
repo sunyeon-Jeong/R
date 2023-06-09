8c376aa0820f0a9fa091b60f03bb9064e2c57d62
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-09 13:30:35 +0900

    Feat : 머신러닝과빅데이터분석(4주차)_데이터가공 #4 - dplyr패키지함수 - 파이프연산자

--- R_MachineLearning&BigData_4.R
+++ R_MachineLearning&BigData_4.R
@@ -1,15 +1,65 @@
 # R_MachineLearning&BigData_4
 # 1. dplyr 패키지함수
+install.packages("dplyr")
+library(dplyr)
+
+mtcars
+
 # nrow : 행 개수 추출
+nrow(mtcars)
+
 # str : 데이터 구조확인
+str(mtcars)
+
 # filter : 행 추출
+filter(mtcars, cyl == 4) # filter(데이터프레임, 조건문)
+filter(mtcars, cyl >= 6 & mpg > 20)
+
 # select : 열 추출
+select(mtcars, am, gear) # select(데이터프레임, 변수명)
+
 # arrange : 정렬
+arrange(mtcars, wt, mpg) # arrange(데이터프레임, 1차기준, 2차기준)
+
 # mutate : 열추가
+mutate(mtcars, year_col = "2023") # mutate(데이터프레임, 추가변수명 = 추가값)
+
+rank(mtcars$mpg)
+mtcars$mpg
+
+mutate(mtcars, mpg_rank = rank(mpg))
+
 # distinct : 중복제거
+mtcars$cyl
+distinct(mtcars, cyl) # distinct(데이터프레임, 컬럼값)
+distinct(mtcars,gear)
+distinct(mtcars,cyl,gear)
+
 # summarise : 데이터요약
+summarise(mtcars, mean(cyl))
+summarise(mtcars, cyl_mean = mean(cyl), cyl_min = min(cyl))
+summarise(mtcars, mean(cyl), min(cyl))
+
+mtcars$cyl
+mean(mtcars$cyl)
+min(mtcars$cyl)
+
 # group_by : 그룹화
+gg_cyl <- group_by(mtcars, cyl) # group_by(데이터프레임, 기준열)
+gg_cyl # tibble타입 : 데이터프레임 후속
+
+summarize(gg_cyl, n()) # 그룹 단위로 데이터가 몇개있는지 확인가능
+
+gg_cyl2 <- group_by(mtcars, cyl)
+summarise(gg_cyl2, n_distinct(gear))
+
 # sample_frac : 샘플추출
+sample_n(mtcars, 10) # 32건중 10건 랜덤추출
+sample_frac(mtcars, 0.5) # 32건중 50% 랜덤추출
+
+# mpg열 순위추가 + 순위를 기준으로 정렬
+mtcars_rank <- mutate(mtcars, mpg_rank = rank(mpg))
+arrange(mtcars_rank, desc(mpg_rank)) # desc : 내림차순정렬
 
 # bind_row : 데이터 상하연결
 # left_join : 데이터 좌우연결
@@ -17,7 +67,9 @@
 # full_join : 변수전체 좌우연결
 
 # 2. 파이프연산자
-# % > % : 수식을 연결해줌
+# % > % : 수식을 연결해줌 (cmd + shift + m)
+mutate(mtcars, mpg_rank = rank(mpg)) %>% 
+    arrange(mpg_rank)
 
 # 3. 데이터구조변경
 # melt() : 열이 긴 데이터 -> 행이 긴 데이터
