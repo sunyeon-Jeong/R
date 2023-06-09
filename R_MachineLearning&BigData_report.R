ed02bcd23abe538795112c8188dfe29719509377
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-09 23:09:12 +0900

    Feat : 머신러닝과빅데이터분석_데이터시각화리포트 #6 - 상권업종대분류명파이차트 - 시군구명막대그래프

--- R_MachineLearning&BigData_report.R
+++ R_MachineLearning&BigData_report.R
@@ -1,1 +1,13 @@
 # R_MachineLearning&BigData_report
+data <- read.csv("/Users/sunyeonjeong/Desktop/coding/R/testdata/store_busan.csv")
+str(data)
+View(data)
+
+library(descr)
+
+# 상권업종대분류명 파이차트
+pie_chart <- table(data$상권업종대분류명)
+pie(pie_chart)
+
+# 시군구명 막대그래프
+freq(data$시군구명, plot = T, main = "부산시 구 별 상권수")
